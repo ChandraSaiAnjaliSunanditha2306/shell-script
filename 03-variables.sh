ad3fa5e3e118a32be14adf546c73e336fee5a913
Anjali <anjalibalusu9@gmail.com>
2026-03-04 22:44:33 +0530

    conversation added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: :$PERSON2"
+echo "$PERSON2: :Hi $PERSON1"
+echo "$PERSON1: : Iam good, how are you?"
+echo "$PERSON2: i am also good"
