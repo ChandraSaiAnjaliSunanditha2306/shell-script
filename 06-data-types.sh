37a6c46449aff2387ca0df939a23be1270d4cd0f
Anjali <anjalibalusu9@gmail.com>
2026-03-05 23:37:37 +0530

    conversation added

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -8,4 +8,4 @@ TIMESTAMP=$(DATE)
 echo "script executed at : $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "SUM Of $NUMBER1 and $NUMBER2 is : "$SUM"
+echo "SUM Of $NUMBER1 and $NUMBER2 is : "$SUM""
