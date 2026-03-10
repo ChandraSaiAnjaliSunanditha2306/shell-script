1a8938fedac9c513c7ee1f37ada27dab580312c9
Anjali <anjalibalusu9@gmail.com>
2026-03-10 23:20:19 +0530

    conversation added

---
+++ 12-colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is $R FAILURE"
+        exit 1
+    else
+        echo -e "$2 is $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo " ERROR: you must have sudo access"
+    exit 1
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y 
+   VALIDATE $? "Installing MYSQL"
+
+else
+    echo -e "MYSQL already $Y installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y 
+    VALIDATE $? "Installing git"
+else
+    echo -e "git already $Y installed"
+fi
